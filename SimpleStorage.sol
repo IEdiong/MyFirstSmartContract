3abdccd1cd1658b2d92decb3f4aedd1d4951debe
IEdiong <ediongi6@hotmail.com>
2024-10-20 23:10:01 +0100

    initial commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25; // This allows any version from 0.8.25 to less than 0.9.0
+
+contract SimpleStorage {
+    uint256 public favoriteNumber; // State variable to store a number
+}
